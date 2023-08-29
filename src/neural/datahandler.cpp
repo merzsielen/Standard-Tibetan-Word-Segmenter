3f984f10930ce1215332c562097450dd06f27e24
Collin Brown <CJBrown7966@gmail.com>
2023-08-29 16:10:11 -0400

    Revert "Tried to Implement Better Context"
    This reverts commit 7c40d3a61f8e5a633d202c768d82025dba43c2c3.

--- src/neural/datahandler.cpp
+++ src/neural/datahandler.cpp
@@ -1,31 +1,29 @@
 ﻿#include "datahandler.h"
 #include "../utility/filehandling.h"
 
-#include <queue>
-
-//double WCharToDouble(wchar_t c)
-//{
-//	if (c == L' ') return DataHandler::wcharOffset - 0.001;
-//	return DataHandler::wcharOffset + (0.001 * ((int)c - DataHandler::blockStart));
-//}
-//
-//wchar_t DoubleToWChar(double d)
-//{
-//	if (d < DataHandler::wcharOffset) return L' ';
-//	return (wchar_t)(((d - DataHandler::wcharOffset) * 1000) + DataHandler::blockStart);
-//}
-
 double WCharToDouble(wchar_t c)
 {
-	if (c == L' ') return 0.0;
-	return (((int)c + 1) - DataHandler::blockStart) / (double)DataHandler::blockLength;
+	if (c == L' ') return DataHandler::wcharOffset - 0.001;
+	return DataHandler::wcharOffset + (0.001 * ((int)c - DataHandler::blockStart));
 }
 
 wchar_t DoubleToWChar(double d)
 {
-	if (d == 0.0) return L' ';
-	return (wchar_t)((int)(d * DataHandler::blockLength) + DataHandler::blockStart - 1);
+	if (d < DataHandler::wcharOffset) return L' ';
+	return (wchar_t)(((d - DataHandler::wcharOffset) * 1000) + DataHandler::blockStart);
 }
+
+//double WCharToDouble(wchar_t c)
+//{
+//	if (c == L' ') return 0.0;
+//	return (((int)c + 1) - DataHandler::blockStart) / (double)DataHandler::blockLength;
+//}
+//
+//wchar_t DoubleToWChar(double d)
+//{
+//	if (d == 0.0) return L' ';
+//	return (wchar_t)((int)(d * DataHandler::blockLength) + 1 + DataHandler::blockStart);
+//}
 
 void DataHandler::Train()
 {
@@ -37,129 +35,148 @@ void DataHandler::Train()
 		then hand the neural network the input and target data.
 	*/
 
-	int charIter = NeuralNet::InputCount;
+	int charIter = 0;
 	int trainIter = 0;
-	int m = NeuralNet::InputCount / 2;
-
-	std::vector<std::pair<double, bool>> corpus;
-
-	for (int i = 0; i < tokenizedCorpus.size() / 8; i++)
-	{
-		wchar_t c = tokenizedCorpus[i];
-		double d = WCharToDouble(c);
-
-		if (c == L' ') corpus[corpus.size() - 1].second = true;
-		else corpus.push_back(std::pair<double, bool>(d, false));
-	}
 
 	std::vector<double> inputs;
 	std::vector<double> targets;
 
-	// First, let's load up our inputs.
-	inputs.push_back(0.0);
-	for (int i = 1; i < NeuralNet::InputCount; i++)
-	{
-		inputs.push_back(corpus[i].first);
-	}
-
 	while (true)
 	{
-		// Have we reached the end of the corpus?
-		if (charIter > corpus.size() - 1) break;
+		// We've reached the end of the corpus and done the
+		// required number of training iterations (which is currently
+		// arbitrary and doesn't really seem to matter).
+		/*if (trainIter >= 500000) break;
+		if (charIter > tokenizedCorpus.size() - 1) charIter = 0;*/
+		if (charIter > tokenizedCorpus.size() - 1) break;
 
-		// Pop the front of the inputs.
-		std::vector<double> temp;
-		for (int i = 1; i < inputs.size(); i++) temp.push_back(inputs[i]);
-		inputs = temp;
+		// Otherwise, keep chugging along.
+		wchar_t c = tokenizedCorpus[charIter];
+		double d = WCharToDouble(c);
 
-		// Add the new back of the inputs.
-		inputs.push_back(corpus[charIter].first);
-
-		// Get the current "center" of the inputs.
-		targets.push_back(corpus[(long long)charIter - m].second);
+		// We only add spaces to the target values.
+		if (c != L' ')
+		{
+			inputs.push_back(d);
+			targets.push_back(0.0);
+		}
+		else if (targets.size() > 0)
+		{
+			targets[targets.size() - 1] = 1.0;
+		}
 
 		charIter++;
 
-		network->Train(inputs, targets, false);
-		targets.clear();
-		trainIter++;
+		// If we've got enough inputs, send it along
+		// to the neural network.
+		if (inputs.size() && inputs.size() % NeuralNet::InputCount == 0)
+		{
+			network->Train(inputs, targets, false);
+			inputs.clear();
+			targets.clear();
+			trainIter++;
+		}
 	}
 
 	std::cout << "Did " << trainIter << " iterations of training.\n";
 	std::cout << "Now, we move on to testing.\n";
 
 	/*
-		Now, we're going to test it quickly.
+		Then, we're going to test it quickly on
+		the first ten characters from the
+		nontokenized corpus.
 	*/
-	int testIter = 100;
-	charIter = 0;// rand() % (corpus.size() - (2 * NeuralNet::InputCount));
-
-	std::vector<double> out;
-	std::vector<double> targs;
-	inputs.clear();
+	/*inputs.clear();
 	targets.clear();
-
 	std::wstring inputText;
 	std::wstring targetText;
 	std::wstring outputText;
 
-	// First, let's load up our inputs.
-	inputs.push_back(0.0);
-	for (int i = charIter; i < charIter + NeuralNet::InputCount; i++)
-	{
-		inputs.push_back(corpus[i].first);
-	}
+	std::wstring testPhrase = L"བཀྲ་ཤིས་བདེ་ལེགསབཀྲ་ཤིས་བདེ་ལེགསབཀྲ་ཤིས་བདེ་ལེགསབཀྲ་ཤིས་བདེ་ལེགསབཀྲ་ཤིས་བདེ་ལེགསབཀྲ་ཤིས་བདེ་ལེགསབཀྲ";
 
-	charIter += NeuralNet::InputCount / 2;
-
+	charIter = 0;
 	while (true)
 	{
-		if (testIter <= 0) break;
+		wchar_t c = testPhrase[charIter];
+		double d = WCharToDouble(c);
 
-		// Pop the front of the inputs.
-		std::vector<double> temp;
-		for (int i = 1; i < inputs.size(); i++) temp.push_back(inputs[i]);
-		inputs = temp;
-
-		// Add the new back of the inputs.
-		inputs.push_back(corpus[charIter].first);
-
-		// Get the current "center" of the inputs.
-		wchar_t c = DoubleToWChar(corpus[(long long)charIter - m].first);
 		inputText += c;
-		targetText += c;
-		outputText += c;
-
-		double isSpace = corpus[(long long)charIter - m].second;
-		targets.push_back(isSpace);
-		targs.push_back(isSpace);
-		if (isSpace) targetText += L" ";
+		if (c != L' ')
+		{
+			inputs.push_back(d);
+		}
 
 		charIter++;
-
-		std::vector<double> o = network->Forward(inputs, false);
-		out.push_back(o[0]);
-		if (o[0]) outputText += L" ";
-
-		targets.clear();
-		testIter--;
+		if (inputs.size() && inputs.size() % NeuralNet::InputCount == 0) break;
 	}
+
+	std::vector out = network->Forward(inputs, false);
+
+	for (int i = 0; i < out.size(); i++)
+	{
+		outputText += inputText[i];
+		if (out[i]) outputText += L" ";
+	}
+
+	WriteFile("datasets/output/test_nontokens.txt", outputText);
+	std::cout << "Job done.\n";*/
+
+	inputs.clear();
+	targets.clear();
+	std::wstring inputText;
+	std::wstring targetText;
+
+	charIter = rand() % 10000;
+	while (true)
+	{
+		wchar_t c = tokenizedCorpus[charIter];
+		double d = WCharToDouble(c);
+
+		targetText += c;
+		if (c != L' ')
+		{
+			inputText += c;
+			inputs.push_back(d);
+			targets.push_back(0.0);
+		}
+		else if (targets.size() > 0)
+		{
+			targets[targets.size() - 1] = 1.0;
+		}
+
+		charIter++;
+		if (inputs.size() && inputs.size() % NeuralNet::InputCount == 0) break;
+	}
+
+	std::vector out = network->Forward(inputs, false);
 
 	double ssr = 0.0;
 	double tss = 0.0;
 
 	double mean = 0.0;
-	for (int i = 0; i < targs.size(); i++) mean += targs[i];
-	mean /= targs.size();
+	for (int i = 0; i < targets.size(); i++) mean += targets[i];
+	mean /= targets.size();
 
 	for (int j = 0; j < out.size(); j++)
 	{
-		ssr += (targs[j] - out[j]) * (targs[j] - out[j]);
-		tss += (targs[j] - mean) * (targs[j] - mean);
+		ssr += (targets[j] - out[j]) * (targets[j] - out[j]);
+		tss += (targets[j] - mean) * (targets[j] - mean);
 	}
 
 	std::cout << "R^2: " << (1.0 - (ssr / tss)) << std::endl;
-	std::wstring test = inputText + L"\n" + targetText + L"\n" + outputText;
+	std::wstring test = inputText + L"\n" + targetText + L"\n";
+
+	for (int i = 0; i < out.size(); i++)
+	{
+		test += inputText[i];
+		if (out[i]) test += L" ";
+	}
+
+	for (int i = 0; i < out.size(); i++) std::cout << out[i] << " / ";
+	std::cout << std::endl;
+	for (int i = 0; i < targets.size(); i++) std::cout << targets[i] << " / ";
+	std::cout << std::endl;
+
 	WriteFile("datasets/output/test_tokens.txt", test);
 	std::cout << "Job done.\n";
 }
